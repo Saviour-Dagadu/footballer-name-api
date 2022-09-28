34fc87ea3432ee3de417f275ceec30eef7aa16e4
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-09-28 07:09:32 +0000

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const { request, response } = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
     const footballer = {
         'ronaldo': {
